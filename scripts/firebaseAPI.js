93b7f862f1f50de95f73be573c32552263a92400
Chun Yip Wong <cy.jimmy.wong@gmail.com>
2021-11-10 01:31:20 -0800

    Added Javascript for customized authentication ui

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -62,3 +62,39 @@ var uiConfig = {
 
 // The start method will wait until the DOM is loaded.
 ui.start('#firebaseui-auth-container', uiConfig);
+
+function signIn() {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // Signed in
+            var user = userCredential.user;
+            // console.log(user.uid)
+            location.href = "main.html"
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+        });
+}
+
+function signUp() {
+    var password = document.getElementById("password").value
+    var email = document.getElementById("email").value
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // Signed in 
+            var user = userCredential.user;
+            console.log("created")
+            db.collection("users").doc(user.uid).set({     //write to firestore. We are using the UID for the ID in users collection
+                name: user.displayName,                    //"users" collection
+                email: user.email                          //with authenticated user's ID (user.uid)
+            }).then(function () {
+                console.log("New user added to firestore");
+                window.location.assign("main.html");       //re-direct to main.html after signup
+            })
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+        });
+}
