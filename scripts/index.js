3113f9513511d7aea3ae37627b4cacc92e857f9d
Chun Yip Wong <cy.jimmy.wong@gmail.com>
2021-11-12 22:59:15 -0800

    hide navbar

--- scripts/index.js
+++ scripts/index.js
@@ -10,8 +10,26 @@ function submitForm(){
     })
 }
 
-
 document.addEventListener("DOMContentLoaded", function(event){
     console.log("Page Loaded!")
     submitForm();
 })
+
+firebase.auth().onAuthStateChanged(user => {
+    // Check if user is signed in:
+    if (user) {
+        // Do something for the current logged-in user here: 
+        console.log(user.uid);
+        //go to the correct user document by referencing to the user uid
+        currentUser = db.collection("users").doc(user.uid);
+        //get the document for current user.
+        currentUser.get()
+            .then(userDoc => {
+                localStorage.setItem('name', userDoc.data().name)
+                localStorage.setItem('email', userDoc.data().email)
+            })
+    } else {
+        // No user is signed in.
+        console.log("no user");
+    }
+});
