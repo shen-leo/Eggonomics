af2ccc90581654f520e25f9a03a7bc812c68f9fd
Chun Yip Wong <cy.jimmy.wong@gmail.com>
2021-11-28 00:47:11 -0800

    add logout button to profile page

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,76 +1,85 @@
-        var currentUser;
+var currentUser;
 
-        function populateInfo() {
-        firebase.auth().onAuthStateChanged((user) => {
+function populateInfo() {
+    firebase.auth().onAuthStateChanged((user) => {
         // Check if user is signed in:
         if (user) {
             //go to the correct user document by referencing to the user uid
             currentUser = db.collection("users").doc(user.uid);
             //get the document for current user.
             currentUser.get().then((userDoc) => {
-            //get the data fields of the user
-            var userName = userDoc.data().name;
-            //console.log(userName)
-            var userEmail = userDoc.data().email;
+                //get the data fields of the user
+                var userName = userDoc.data().name;
+                //console.log(userName)
+                var userEmail = userDoc.data().email;
 
-            //if the data fields are not empty, then write them in to the form.
-            if (userName != null) {
-                document.getElementById("nameInput").value = userName;
-            }
-            if (userEmail != null) {
-                document.getElementById("emailInput").value = userEmail;
-            }
+                //if the data fields are not empty, then write them in to the form.
+                if (userName != null) {
+                    document.getElementById("nameInput").value = userName;
+                }
+                if (userEmail != null) {
+                    document.getElementById("emailInput").value = userEmail;
+                }
             });
         } else {
             // No user is signed in.
             console.log("No user is signed in");
         }
-        });
-        }
+    });
+}
 
-        populateInfo();
+populateInfo();
 
-        function editUserInfo(){
-            //console.log("edit is clicked")
-            document.getElementById("personalInfoFields").disabled = false;
-        }
+function editUserInfo() {
+    //console.log("edit is clicked")
+    document.getElementById("personalInfoFields").disabled = false;
+}
 
-        function saveUserInfo(){
-            //console.log("save is clicked")
-            
-            //grab values from the form that the user inserted in each field
-            username = document.getElementById('nameInput').value;
-            email = document.getElementById('emailInput').value;
+function saveUserInfo() {
+    //console.log("save is clicked")
 
-        //   console.log("values are: ", name, school, city)
-        
-        // write the values in database
-        currentUser.update({
-            name: username,
-            email: email
+    //grab values from the form that the user inserted in each field
+    username = document.getElementById('nameInput').value;
+    email = document.getElementById('emailInput').value;
 
-        })
-        document.getElementById("personalInfoFields").disabled = true;
-        }     
+    //   console.log("values are: ", name, school, city)
+
+    // write the values in database
+    currentUser.update({
+        name: username,
+        email: email
+
+    })
+    document.getElementById("personalInfoFields").disabled = true;
+}
 
 
-        // Confirmation Modal
-        var modal = document.getElementById("myModal");
+// Confirmation Modal
+var modal = document.getElementById("myModal");
 
-        var btn = document.getElementById("save");
+var btn = document.getElementById("save");
 
-        var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close")[0];
 
-        btn.onclick = function() {
-        modal.style.display = "block";
-        }
+btn.onclick = function () {
+    modal.style.display = "block";
+}
 
-        span.onclick = function() {
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
         modal.style.display = "none";
-        }
+    }
+}
 
-        window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-        }
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        window.location.assign("login.html")
+    }).catch((error) => {
+        // An error happened.
+    });
+}
