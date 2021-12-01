2aa7d7b804f8c19c49182978baa179eb7d0fb29c
Chun Yip Wong <cy.jimmy.wong@gmail.com>
2021-11-30 23:49:30 -0800

    added download function to main shopping list page

--- scripts/all_shopping_list.js
+++ scripts/all_shopping_list.js
@@ -1,11 +1,5 @@
 const cardContainer = document.getElementById("card-container")
 
-// let hikeID = localStorage.getItem("hikeID");
-//     console.log(hikeID)
-//     function setHikeData(id) {
-//         localStorage.setItem('hikeID', id);
-//     }
-
 function openList(listId) {
   localStorage.setItem('listId', listId);
   localStorage.setItem('newList', false);
@@ -14,10 +8,10 @@ function openList(listId) {
 
 function newList() {
   let newId = "0";
-  if(!cardContainer.lastChild){
+  if (!cardContainer.lastChild) {
     newId = "1";
   }
-  else{
+  else {
     newId = (parseInt(cardContainer.lastChild.id) + 1).toString();
   }
   localStorage.setItem('listId', newId);
@@ -26,19 +20,16 @@ function newList() {
 }
 
 
-function populate() {
+async function populate() {
   firebase.auth().onAuthStateChanged(user => {
     if (user) {
       db.collection("shoppinglist").doc(user.uid).collection("lists").get().then(snap => {
         snap.forEach(doc => {
           let listName = doc.data().name;
-          if (!doc.data().name){
+          if (!doc.data().name) {
             listName = "unnamed shopping list"
           }
           let listImg = doc.data().img;
-          if (!doc.data().img){
-            listImg = "https://www.gannett-cdn.com/media/2021/07/09/USATODAY/usatsports/fresh-grocery-assortment.jpg"
-          }
 
           let card = document.createElement("div")
           card.setAttribute('id', `${doc.id}`)
@@ -51,18 +42,21 @@ function populate() {
 
           let cardImg = document.createElement("img")
           cardImg.setAttribute('class', "card-img")
-          cardImg.setAttribute('src', `${listImg}`)
-
-          // let deleteButton = document.createElement("button")
-          // deleteButton.setAttribute('onclick', `deleteItem("${card.id}")`)
-          // deleteButton.setAttribute('class', "delete-btn")
+          if (!doc.data().img) {
+            listImg = "https://www.gannett-cdn.com/media/2021/07/09/USATODAY/usatsports/fresh-grocery-assortment.jpg"
+            cardImg.setAttribute('src', `${listImg}`)
+          }
+          else {
+            cardImg.setAttribute('data-img', `${doc.data().img}`)
+          }
 
           card.appendChild(cardImg)
           card.appendChild(cardName)
-          // card.appendChild(deleteButton)
 
           cardContainer.appendChild(card)
         })
+      }).then(() => {
+        getImg()
       })
     }
   })
@@ -70,24 +64,32 @@ function populate() {
 populate()
 
 
-var storageRef = firebase.storage().ref();
-    storageRef.child('backiee-35593.jpg').getDownloadURL()
-      .then((url) => {
-        // `url` is the download URL for 'images.jpg'
+function getImg() {
+  var storageRef = firebase.storage().ref();
+  images = document.querySelectorAll(".card-img")
+  images.forEach(function (image) {
+    if (image.dataset.img) {
+      storageRef.child(image.dataset.img).getDownloadURL()
+        .then((url) => {
+          // `url` is the download URL for 'images.jpg'
 
-        // This can be downloaded directly:
-        var xhr = new XMLHttpRequest();
-        xhr.responseType = 'blob';
-        xhr.onload = (event) => {
-          var blob = xhr.response;
-        };
-        xhr.open('GET', url);
-        xhr.send();
+          // This can be downloaded directly:
+          var xhr = new XMLHttpRequest();
+          xhr.responseType = 'blob';
+          xhr.onload = (event) => {
+            var blob = xhr.response;
+          };
+          xhr.open('GET', url);
+          xhr.send();
 
-        // Or inserted into an <img> element
-        var img = document.getElementById('test');
-        img.setAttribute('src', url);
-      })
-      .catch((error) => {
-        // Handle any errors
-      });
+          // Or inserted into an <img> element
+          image.setAttribute("src", url)
+          console.log(url)
+        })
+        .catch((error) => {
+          console.log("error")
+          // Handle any errors
+        });
+    }
+  })
+}
